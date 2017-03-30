c717304227ec6ae936f5f7c529ae12889e9e6353
Zhen-smc <albert.lz07@gmail.com>
2017-03-30 14:12:56 +0300

    list_chi.sh using input vars

--- list_chi.sh
+++ list_chi.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # make input file for the FM2Cantera, list all the flamelet resutls of FlameMaster
-echo OutMethane/CH4_p01_0chi > input;
-echo tf0291to0294 >> input;
-ls OutMethane/CH4_p01_0chi*tf0291to0294 | \
-    sed -e 's@OutMethane/CH4_p01_0chi@@g' -e 's@tf0291to0294@@g' | \
+echo $1 > input;
+echo $2 >> input;
+ls $1*$2 | \
+    sed -e "s@$1@@g" -e "s@$2@@g" | \
     sort -g >> input;
